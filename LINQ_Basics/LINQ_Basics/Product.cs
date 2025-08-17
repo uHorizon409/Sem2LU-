07ba2f25229d669b0436e2bff8ff2ab719d58f60
ludaniel <ludaniel@iie.ac.za>
2025-08-17 20:19:12 +0200

    Linq started

---
+++ LINQ_Basics/LINQ_Basics/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Basics
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+    }
+    public class Person
+    {
+        //properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    }
