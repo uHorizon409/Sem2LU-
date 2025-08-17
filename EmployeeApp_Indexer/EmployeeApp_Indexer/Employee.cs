c979bef01184450a93518b5c7cb148a0ebbf2668
ludaniel <ludaniel@iie.ac.za>
2025-08-17 19:53:03 +0200

    Indexer Lecture 2

---
+++ EmployeeApp_Indexer/EmployeeApp_Indexer/Employee.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp_Indexer
+{
+    class Employee
+    {
+        //declaring attributes
+        double salary;
+        string empNumber, jobTitle, name, surname;
+
+        // declaring properties
+        
+        public string Name 
+        {
+            get 
+            {
+                return name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string EmpNumber
+        {
+            get
+            {
+                return empNumber;
+            }
+            set
+            {
+                this.empNumber = value;
+            }
+        }
+        public string JobTitle
+        {
+            get
+            {
+                return jobTitle;
+            }
+            set
+            {
+                this.jobTitle = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                this.surname = value;
+            }
+        }
+        public double Salary
+            {
+            get 
+            { 
+                return this.salary;
+
+            }
+            set
+            {
+                this.salary = value;
+            } 
+            }
+        public Employee(string empnum, string title, string nme, string snme, double sal) 
+        {
+            this.empNumber = empnum;
+            this.jobTitle = title;
+            this.name = nme;
+            this.surname = snme;
+            this.salary = sal;
+            
+        }
+        //declare indexer that searches using the indexer value
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0) 
+                    return this.empNumber;
+                else if (index == 1)
+                    return this.jobTitle;
+                else if (index == 2)
+                    return this.name;
+                else if (index == 3)
+                    return this.surname;
+                else if (index == 4)
+                    return this.salary;
+                return null;
+
+            }
+            set 
+            {
+                if (index == 0)
+                    this.empNumber = (string)value;
+                else if (index == 1)
+                    this.jobTitle = (string)value;
+                else if (index == 2)
+                    this.name = (string)value;
+                else if (index ==3)
+                    this.surname = (string)value;
+                else 
+                    this.salary = (double)value;
+                
+            }
+        }
+
+        //declare an indexer using string values
+    }
+}
---
+++ EmployeeApp_Indexer/EmployeeApp_Indexer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+
+namespace EmployeeApp_Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("emp001", "Director", "Bagezile", "Zuma", 5000);
+            //Console.WriteLine("Employee Number : " + emp[0]);
+            //Console.WriteLine("Employee Title : " + emp[1]);
+            //Console.WriteLine("Employee Name : " + emp[2]);
+            //Console.WriteLine("Employee Surname: " + emp[3]);
+            //Console.WriteLine("Employee Salary : " + emp[4]);
+
+            for (int i = 0; i < emp i++)
+            {
+                Console.WriteLine(emp[i]);
+            }
+            // Print using for loop
+
+            //string[] labels = {
+            //        "Employee Number : ",
+            //        "Employee Title : ",
+            //        "Employee Name : ",
+            //        "Employee Surname: ",
+            //        "Employee Salary : "
+            //    };
+
+            //string[] values = {
+            //    emp.EmpNumber,
+            //    emp.JobTitle,
+            //    emp.Name,
+            //    emp.Surname,
+            //    emp.Salary.ToString()
+            //};
+
+
+            //for (int i = 0; i < labels.Length; i++)
+            //{
+            //    Console.WriteLine(labels[i] + values[i]);
+            //}
+        }
+    }
+}
